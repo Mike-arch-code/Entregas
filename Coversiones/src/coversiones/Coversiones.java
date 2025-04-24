eb146120c4420c00c67447e63439be0cfc8ed041
SantiagoBR17 <jsantiago.br.17@gmail.com>
2025-04-23 22:53:55 -0500

    Creacion de objetos con imprecion tipo de datos

--- Coversiones/src/coversiones/Coversiones.java
+++ Coversiones/src/coversiones/Coversiones.java
@@ -15,6 +15,16 @@ public class Coversiones {
      */
     public static void main(String[] args) {
         // TODO code application logic hereasdasd
+        String Nombre = "Santiago";
+        Integer Edad = 23; 
+        Float Estatura = 1.69f;
+        Boolean EstudianteActivo = true;
+
+        System.out.println("Nombre: " + Nombre + " (Tipo: " + Nombre.getClass().getSimpleName() + ")");
+        System.out.println("Edad: " + Edad + " (Tipo: " + Edad.getClass().getSimpleName() + ")");
+        System.out.println("Estatura: " + Estatura + " (Tipo: " + Estatura.getClass().getSimpleName() + ")");
+        System.out.println("EstudianteActivo: " + EstudianteActivo + " (Tipo: " + EstudianteActivo.getClass().getSimpleName() + ")");
+
     }
     
 }
